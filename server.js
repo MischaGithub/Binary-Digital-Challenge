17bd6df1ee3bd84f0fa4634fe1860d4154082fc1
MischaGithub <mischa.achmat120212@gmail.com>
2020-09-11 14:38:52 +0200

    prepare for deploy

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("image-slider/build"));
   
     // App get
-    app.get("*", (req, res) =>
+    app.get("/", (req, res) =>
       res.sendFile(path.resolve(__dirname, "image-slider", "build", "index.html"))
     );
   }
