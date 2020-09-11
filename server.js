75efbb8878bb83a95a864eb2651b3d3bc6e1501e
MischaGithub <mischa.achmat120212@gmail.com>
2020-09-11 14:46:57 +0200

    prepare for deployment

--- server.js
+++ server.js
@@ -13,16 +13,16 @@ app.use(express.json({ extended: false}));
 // Defining Route
 app.use("/api/images", require("./route/images"));
 
-// Serve static assest in production
-if (process.env.NODE_ENV === "production") {
-    // Set static folder
-    app.use(express.static("image-slider/build"));
+// // Serve static assest in production
+// if (process.env.NODE_ENV === "production") {
+//     // Set static folder
+//     app.use(express.static("image-slider/build"));
   
-    // App get
-    app.get("*", (req, res) =>
-      res.sendFile(path.resolve(__dirname, "image-slider", "build", "index.html"))
-    );
-  }
+//     // App get
+//     app.get("*", (req, res) =>
+//       res.sendFile(path.resolve(__dirname, "image-slider", "build", "index.html"))
+//     );
+//   }
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
